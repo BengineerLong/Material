06b5d93a18fabe2f662baa5e7718d43eaabcdd8d
BengineerLong <75713313+BengineerLong@users.noreply.github.com>
2020-12-08 21:28:34 -0700

    Update MEAT.py

--- MEAT.py
+++ MEAT.py
@@ -1,14 +1,1 @@
-#Meatwad get the honies see, make the money see, she be driving in my car, livin' like a star
-
-
-
-
-
-
-
-
-
-
-
-
-..Ice on my fingers, on my toes n' on my toys!
+print(#Meatwad get the honies see, make the money see, she be driving in my car, livin' like a star..Ice on my fingers, on my toes n' on my toys!)
